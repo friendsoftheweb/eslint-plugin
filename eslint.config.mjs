3b0db5b48e0e89ccdf57a7bdbb386f9890b3e09b
Dan Martens <dan@friendsoftheweb.com>
2026-01-06 16:50:45 -0500

    Add ESLint recommended to config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,23 @@
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
 import eslintPlugin from 'eslint-plugin-eslint-plugin';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
   {
-    ignores: ['.yarn/**', 'node_modules/**'],
+    ignores: ['.yarn/**', 'dist/**', 'node_modules/**'],
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.node },
+    },
   },
   eslintPlugin.configs.recommended,
+  js.configs.recommended,
   {
     rules: {
       'eslint-plugin/require-meta-docs-description': 'error',
+      'no-undef': ['error', { typeof: true }],
     },
   },
 ]);
--- test.mjs
+++ test.mjs
@@ -1,5 +1,4 @@
 import fs from 'node:fs';
-import path from 'node:path';
 
 import { RuleTester } from 'eslint';
 import sinon from 'sinon';
