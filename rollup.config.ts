c231f861578456711dba1cb8ebe124656afece68
Dan Martens <dan@friendsoftheweb.com>
2026-01-06 16:45:58 -0500

    Update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,24 +1,43 @@
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 
-export default defineConfig({
-  input: ['src/index.mjs'],
-  output: [
-    {
-      dir: 'dist/cjs',
-      format: 'cjs',
-      preserveModules: true,
-      exports: 'auto',
-      sourcemap: true,
-    },
-    {
-      dir: 'dist/esm',
-      format: 'esm',
-      preserveModules: true,
-      exports: 'auto',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript()],
-  external: ['node:fs', 'node:path', 'postcss', 'postcss-selector-parser'],
-});
+const external = ['node:fs', 'node:path', 'postcss', 'postcss-selector-parser'];
+
+export default defineConfig([
+  {
+    input: 'src/index.mjs',
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        preserveModules: true,
+        exports: 'auto',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        outDir: 'dist/esm',
+      }),
+    ],
+    external,
+  },
+  {
+    input: 'src/index.mjs',
+    output: [
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        preserveModules: true,
+        exports: 'auto',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        outDir: 'dist/cjs',
+      }),
+    ],
+    external,
+  },
+]);
