48257b80435fc97e5b94690942ad7ba8c79c8b18
Dan Martens <dan@friendsoftheweb.com>
2026-01-07 09:17:43 -0600

    Ban importing functions from lodash (non es) (#6)
    * Ban importing functions from lodash (non es)
    
    * Fix invalid autocorrect for lodash/fp
    
    * Maintain quote type

---
+++ src/rules/ban-lodash-import.mjs
@@ -0,0 +1,55 @@
+/** @type {import('eslint').JSRuleDefinition} */
+export default {
+  meta: {
+    type: 'problem',
+    fixable: 'code',
+    docs: {
+      description:
+        'enforce importing functions from `lodash-es` instead of `lodash`',
+    },
+    schema: [],
+    messages: {
+      invalidImport:
+        'Functions must be imported from "lodash-es" instead of "lodash"',
+    },
+  },
+  create(context) {
+    return {
+      ImportDeclaration(node) {
+        if (
+          typeof node.source.value !== 'string' ||
+          (node.source.value !== 'lodash' &&
+            !node.source.value.startsWith('lodash/'))
+        ) {
+          return;
+        }
+
+        context.report({
+          node,
+          messageId: 'invalidImport',
+          fix(fixer) {
+            if (typeof node.source.value !== 'string') {
+              return null;
+            }
+
+            if (node.source.value === 'lodash/fp') {
+              return null; // no automatic fix for fp imports
+            }
+
+            const newImportPath =
+              node.source.value === 'lodash'
+                ? 'lodash-es'
+                : node.source.value.replace(/^lodash\//, 'lodash-es/');
+
+            const quote = node.source.raw[0]; // preserve original quote style
+
+            return fixer.replaceText(
+              node.source,
+              `${quote}${newImportPath}${quote}`,
+            );
+          },
+        });
+      },
+    };
+  },
+};
